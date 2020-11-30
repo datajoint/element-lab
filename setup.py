d21a872100f9cdaf450681982eb1eee03ed233b3
Thinh Nguyen <thinh@vathes.com>
2020-11-30 12:13:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ import sys
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
-Canonical DataJoint for lab management
+DataJoint Elements for Lab Management
 """
 
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='canonical-lab-management',
+    name='elements-lab',
     version='0.0.1',
-    description="Canonical Lab Management Pipeline",
+    description="DataJoint Elements for Lab Management",
     long_description=long_description,
     author='DataJoint NEURO',
     author_email='info@vathes.com',
     license='MIT',
-    url='https://github.com/vathes/canonical-lab-management',
+    url='https://github.com/datajoint/elements-lab',
     keywords='neuroscience lab manamgement datajoint',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=requirements,
