465084dff15a90b5fb7fde9146414674fe0a6c72
Chris Broz <CBroz@datajoint.com>
2022-01-12 12:52:35 -0600

    refactor nwb.py: only execute funcs if relevant input

--- element_lab/export/nwb.py
+++ element_lab/export/nwb.py
@@ -1,61 +1,52 @@
 from element_lab import lab
 
 
-def lab_to_nwb_dict(lab_key=None):
+def lab_to_nwb_dict(lab_key):
     """
     Generate a dictionary containing all relevant lab and institution info
     :param lab_key: Key specifying one entry in element_lab.lab.Lab
     :return: dictionary with NWB parameters
     """
-    if lab_key is not None:
-        lab_info = (lab.Lab & lab_key).fetch1()
-        return dict(
-            institution=lab_info.get('institution', ''),
-            lab=lab_info.get('lab_name', '')
-        )
-    else:
-        return {}
+    lab_info = (lab.Lab & lab_key).fetch1()
+    return dict(
+        institution=lab_info.get('institution', ''),
+        lab=lab_info.get('lab_name', '')
+    )
 
 
-def project_to_nwb_dict(project_key=None):
+def project_to_nwb_dict(project_key):
     """
     Generate a dictionary object containing relevant project information
         (e.g., experimental description, related publications, etc.).
     :param project_key: Key specifying one entry in element_lab.lab.Project
     :return: dictionary with NWB parameters
     """
-    if project_key is not None:
-        proj_info = (lab.Project & project_key).fetch1()
-        proj_keyw = (lab.Project.Keywords() & project_key
-                     ).fetch('keyword').tolist()
-        proj_pubs = (lab.Project.Publication() & project_key
-                     ).fetch('publication').tolist()
-        return dict(
-            experiment_description=(proj_info.get('project_description', '')),
-            keywords=proj_keyw,
-            related_publications=proj_pubs
-        )
-    else:
-        return {}
+    proj_info = (lab.Project & project_key).fetch1()
+    proj_keyw = (lab.Project.Keywords() & project_key
+                 ).fetch('keyword').tolist()
+    proj_pubs = (lab.Project.Publication() & project_key
+                 ).fetch('publication').tolist()
+    return dict(
+        experiment_description=(proj_info.get('project_description', '')),
+        keywords=proj_keyw,
+        related_publications=proj_pubs
+    )
 
 
-def protocol_to_nwb_dict(protocol_key=None):
+def protocol_to_nwb_dict(protocol_key):
     """
     Generate a dictionary object containing all protocol title and notes.
     :param protocol_key: Key specifying one entry in element_lab.lab.Protocol
     :return: dictionary with NWB parameters
     """
-    if protocol_key is not None:
-        prot_info = (lab.Protocol & protocol_key).fetch1()
-        return dict(
-            # data_collection='',
-            protocol=(prot_info['protocol']
-                      if 'protocol' in prot_info else ''),
-            notes=(prot_info['protocol_description']
-                   if 'project_description' in prot_info else '')
-        )
-    else:
-        return {}
+    prot_info = (lab.Protocol & protocol_key).fetch1()
+    return dict(
+        # data_collection='',
+        protocol=(prot_info['protocol']
+                  if 'protocol' in prot_info else ''),
+        notes=(prot_info['protocol_description']
+               if 'project_description' in prot_info else '')
+    )
 
 
 def elementlab_nwb_dict(lab_key=None, project_key=None, protocol_key=None):
@@ -75,19 +66,23 @@ def elementlab_nwb_dict(lab_key=None, project_key=None, protocol_key=None):
     :return: dictionary with NWB parameters
     """
     # Validate input
-    if lab_key is not None: 
-        assert len(lab.Lab & lab_key) == 1,  'Multiple labs error! '\
-            'The lab_key should specify only one lab.'
+    assert any([lab_key, project_key, protocol_key]), 'Must specify one key.'
+    assert lab_key is None or len(lab.Lab & lab_key) == 1, \
+        'Multiple labs error! The lab_key should specify only one lab.'
     assert project_key is None or len(lab.Project & project_key) == 1, \
-        'Multiple projects error! The project_key should specify only one project.'
+        'Multiple projects error! The project_key should specify only one '\
+        'project.'
     assert protocol_key is None or len(lab.Protocol & protocol_key) == 1, \
-        'Multiple protocols error! protocol_key should specify only one protocol.'
+        'Multiple protocols error! protocol_key should specify only one '\
+        'protocol.'
 
-    elem_info = dict(
-        lab_to_nwb_dict(lab_key),
-        **project_to_nwb_dict(project_key),
-        **protocol_to_nwb_dict(protocol_key)
-        )
+    elem_info = dict()
+    if lab_key:
+        elem_info.update(**lab_to_nwb_dict(lab_key))
+    if project_key:
+        elem_info.update(**project_to_nwb_dict(project_key))
+    if protocol_key:
+        elem_info.update(**protocol_to_nwb_dict(protocol_key))
 
     # Drop blank entries
     elem_info = {k: v for k, v in elem_info.items() if v}
