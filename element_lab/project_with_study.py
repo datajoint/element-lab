f69d7c3b1d7ecb40dd1d83481e8d40753ce018e8
Joseph Burling <joseph@datajoint.com>
2022-08-26 13:34:44 -0500

    docs: comment changes and activation

--- element_lab/project_with_study.py
+++ element_lab/project_with_study.py
@@ -1,9 +1,23 @@
+import importlib
+import inspect
+
 import datajoint as dj
+
+from element_lab import lab
 
 schema = dj.Schema()
 
+_linking_module = None
 
-def activate(schema_name, create_schema=True, create_tables=True, linking_module=None):
+
+def activate(
+    project_schema_name,
+    lab_schema_name,
+    *,
+    create_schema=True,
+    create_tables=True,
+    linking_module=None,
+):
     """
     activate(schema_name, create_schema=True, create_tables=True, linking_module=None)
         :param schema_name: schema name on the database server to activate the
@@ -14,22 +28,39 @@ def activate(schema_name, create_schema=True, create_tables=True, linking_module
                               database if they do not yet exist.
         :param linking_module: a module (or name) containing the required
                             dependencies to activate the `event` element:
-        Upstream tables:
-            + User: table defining user/personnel/experimenter to be associated with Project.
     """
+    if isinstance(linking_module, str):
+        linking_module = importlib.import_module(linking_module)
+    assert inspect.ismodule(linking_module), (
+        "The argument 'dependency' must" + " be a module or module name"
+    )
+
+    global _linking_module
+    _linking_module = linking_module
+
+    lab.activate(
+        lab_schema_name,
+        create_schema=create_schema,
+        create_tables=create_tables,
+        linking_module=_linking_module,
+    )
+
     schema.activate(
-        schema_name, create_schema=create_schema, create_tables=create_tables
+        project_schema_name,
+        create_schema=create_schema,
+        create_tables=create_tables,
+        add_objects=_linking_module.__dict__,
     )
 
 
 @schema
 class Project(dj.Manual):
-    definition = """# Information about the project
+    definition = """# Top-level grouping of studies and experiments to investigate a scientific question
     project_id                               : VARCHAR(24)                 # abbreviated project name
     ---
-    project_title                            : VARCHAR(1024)               # full project title and description
+    project_title                            : VARCHAR(1024)               # full project title and/or description
     project_start_date                       : DATE                        # the start of the project
-    project_end_date                         : DATE                        # the end date of the project
+    project_end_date=NULL                    : DATE                        # the end date of the project
     project_url=''                           : VARCHAR(512)                # URL of the project repository
     project_keywords=''                      : VARCHAR(1024)               # comma-separated list of keywords describing the project
     project_comment=''                       : VARCHAR(1024)               # additional notes on the project
@@ -38,14 +69,14 @@ class Project(dj.Manual):
     class Personnel(dj.Part):
         definition = """# List of individuals involved in a project
         -> master
-        -> User
+        -> lab.User
         """
 
 
 @schema
 class Study(dj.Manual):
-    definition = """# Collection of tasks, experiments, or aims under a single project
-    study_name                               : VARCHAR(128)                # full study name, e.g., 'Navigation tasks', or 'Aim 1'
+    definition = """# A research protocol or set of experiments designed to address a specific aim
+    study_name                               : VARCHAR(128)                # full study name, e.g., 'perceptual response tasks', or 'Aim 1'
     -> Project
     ---
     study_description=''                     : VARCHAR(1024)               # description of study goals, objectives, and/or methods
@@ -60,12 +91,12 @@ class Study(dj.Manual):
 
 @schema
 class Experiment(dj.Manual):
-    definition = """# Experimental tasks and their associated lab and study
-    experiment_uuid                 : UUID                        # unique identifier of the experiment
+    definition = """# Experimental tasks and protocols and their associated lab and study
+    experiment_uuid                          : UUID                        # unique identifier of the experiment
     ---
-    experiment_name=NULL            : VARCHAR(32)                 # experiment name or identifier
-    experiment_description=''       : VARCHAR(1024)               # short description of the focus of the experiment
+    experiment_name=NULL                     : VARCHAR(32)                 # experiment name or identifier
+    experiment_description=''                : VARCHAR(1024)               # short description of the focus of the experiment
     -> Study
-    -> Lab
-    -> [nullable] Protocol
+    -> lab.Lab
+    -> [nullable] lab.Protocol
     """
